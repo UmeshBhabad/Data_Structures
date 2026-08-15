cddaa78182f700d711aae6aa2454a44cfa7076cf
Umesh Bhabad <umeshbhabad9@gmail.com>
2026-08-16 02:49:44 +0530

    Time: 119 ms (10.40%) | Memory: 48.1 MB (9.54%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,56 @@
+class Solution
+{
+    public String minWindow(String s, String t)
+    {
+        int n = s.length();
+        int m = t.length();
+
+        // Map<Character, Integer> map = new HashMap<>();
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        Map<Character, Integer> sMap = new HashMap<>();
+
+        for(char c : t.toCharArray())
+        {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        String res = "";
+
+        int l = 0, r = 0;
+
+        int count = m;
+        int minLength = Integer.MAX_VALUE;
+
+        for(r = 0; r < n; r++)
+        {
+            char ch = s.charAt(r);
+
+            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+
+            if(tMap.containsKey(ch) && sMap.get(ch) <= tMap.get(ch))
+            {
+                count--;
+            }
+
+            while(count == 0)
+            {
+                if(r - l + 1 < minLength)
+                {
+                    minLength = r - l + 1;
+                    res = s.substring(l , r + 1);
+                }
+
+                char leftCh = s.charAt(l);
+                sMap.put(leftCh, sMap.get(leftCh) - 1);
+
+                if(tMap.containsKey(leftCh) && sMap.get(leftCh) < tMap.get(leftCh))
+                {
+                    count++;
+                }
+                l++;
+            }
+        }
+        return res;
+    }
+}
