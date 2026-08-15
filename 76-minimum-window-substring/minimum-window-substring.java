6eb5f086e9f45de8d76733428fa4ee8f6cc50633
Umesh Bhabad <umeshbhabad9@gmail.com>
2026-08-16 02:51:00 +0530

    Time: 122 ms (8.99%) | Memory: 47.8 MB (18.70%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,6 +1,13 @@
 class Solution
 {
-    public String minWindow(String s, String t)
+    static
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            minWindow(new String(), new String());
+        }
+    }
+    public static String minWindow(String s, String t)
     {
         int n = s.length();
         int m = t.length();
